58eee57c00cf14710ca50f5b6de57d59d062022a
Manikandan Krishnasamy <manikandank8286@gmail.com>
2023-09-28 06:40:39 +0000

    #11 Install provider, create and destory S3 Bucket

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+        aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
   }
 }
 
@@ -12,9 +16,19 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length  = 16
+  lower = true
+  upper = false
+  length  = 32
   special = false
  
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+
+resource "aws_s3_bucket" "example" {
+  #https://docs.aws.amazon.com/console/s3/bucket-naming
+
+  bucket = random_string.bucket_name.result
+
 }
 
 output "random_bucket_name"{
